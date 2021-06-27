4a140f70c141a61fbc17501fe9fadb86b280ffd1
ahmetbozaci <ahmt9417@gmail.com>
2021-06-27 23:02:11 +0300

    Add function for navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu(){
+  let menu = document.getElementById("desktop-nav");
+  if(menu.style.display === "none"){
+    menu.style.display = "block"
+  } else {
+    menu.style.display = "none"
+  }
+}
