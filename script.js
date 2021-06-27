6c90deaf8c14e4435a159009af2fac27e4fea24f
ahmetbozaci <ahmt9417@gmail.com>
2021-06-28 01:48:33 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function openMenu(){
-  let menu = document.getElementById("desktop-nav");
-  if(menu.style.display === "none"){
-    menu.style.display = "block"
+// eslint-disable-next-line no-unused-vars
+function openMenu() {
+  const menu = document.getElementById('desktop-nav');
+  if (menu.style.display === 'none') {
+    menu.style.display = 'block';
   } else {
-    menu.style.display = "none"
+    menu.style.display = 'none';
   }
 }
